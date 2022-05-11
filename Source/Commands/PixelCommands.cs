53b4de50367707a33fec82e052d1d2ad5cf775b2
Helodity <43509068+Helodity@users.noreply.github.com>
2022-05-10 22:09:48 -0400

    Cleanup of PixelModule

--- Source/Commands/PixelCommands.cs
+++ Source/Commands/PixelCommands.cs
@@ -2,71 +2,11 @@
 
 [SlashCommandGroup("pixel", "r/place but inside a discord bot")]
 class PixelCommands : ApplicationCommandModule {
-
-    public SKSurface GetPixelSurface(InteractionContext ctx, int anchorX, int anchorY, int distance) {
-        var mapData = Bot.Modules.Pixel.GetPixelMap(ctx.Guild.Id);
-        int scale = 500 / distance;
-
-        SKImageInfo imageInfo = new SKImageInfo(500, 500);
-        SKSurface surface = SKSurface.Create(imageInfo);
-        SKCanvas canvas = surface.Canvas;
-
-        for(int x = 0; x < distance; x++) {
-            for(int y = 0; y < distance; y++) {
-                int curX = x + anchorX;
-                int curY = y + anchorY;
-                SKColor color;
-                if(curX < 0 || curX >= PixelModule.MapWidth || curY < 0 || curY >= PixelModule.MapHeight) {
-                    color = SKColors.Black.WithAlpha(100);
-                } else if(!PixelModule.PixelDict.TryGetValue((uint)mapData.PixelState[curX, curY], out color)) {
-                    color = SKColors.White;
-                }
-                SKPaint paint = new SKPaint();
-                paint.Color = color;
-                paint.Style = SKPaintStyle.Fill;
-                paint.IsAntialias = false;
-
-                canvas.DrawRect(x * scale, y * scale, scale, scale, paint);
-            }
-        }
-
-        return surface;
-    }
-
-    public void AddInteractUI(SKSurface surface, int zoom, PixelModule.PixelEnum colorEnum) {
-        if(PixelModule.PixelDict.TryGetValue((uint)colorEnum, out SKColor color))
-            DrawCenterPixel(surface.Canvas, zoom, color.WithAlpha(100));
-        AddOutline(surface.Canvas, zoom, 2, SKColors.Silver);
-    }
-    void AddOutline(SKCanvas canvas, int zoom, int thickness, SKColor color) {
-        SKPaint paint = new SKPaint();
-        paint.Color = color;
-        paint.Style = SKPaintStyle.Stroke;
-        paint.StrokeWidth = thickness;
-        paint.IsAntialias = false;
-
-        int pixelSize = 500 / zoom;
-
-        canvas.DrawRect(pixelSize * (zoom / 2), pixelSize * (zoom / 2), pixelSize, pixelSize, paint);
-    }
-
-    void DrawCenterPixel(SKCanvas canvas, int zoom, SKColor color) {
-        SKPaint paint = new SKPaint();
-        paint.Color = color;
-        paint.Style = SKPaintStyle.Fill;
-        paint.IsAntialias = false;
-
-        int pixelSize = 500 / zoom;
-
-        canvas.DrawRect(pixelSize * (zoom / 2), pixelSize * (zoom / 2), pixelSize, pixelSize, paint);
-    }
-
     [SlashCommand("view", "look at the current canvas")]
     public async Task ViewCanvas(InteractionContext ctx) {
         await ctx.CreateResponseAsync($"Loading...", true);
-        SKSurface canvas = GetPixelSurface(ctx, 0, 0, PixelModule.MapHeight);
         string imagePath = $"PixelImages/img{ctx.User.Id}.png";
-        canvas.Snapshot().SaveToPng(imagePath);
+        Bot.Modules.Pixel.CreateImage(ctx, PixelModule.MapWidth / 2, PixelModule.MapHeight / 2, PixelModule.MapHeight);
         using(var fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read)) {
             await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().AddFile(imagePath, fs).AsEphemeral());
         }
@@ -74,8 +14,8 @@ class PixelCommands : ApplicationCommandModule {
     }
     [SlashCommand("interact", "look with buttons")]
     public async Task Interact(InteractionContext ctx,
-        [Option("x", "x to start at")] long start_x = 5,
-        [Option("y", "y to start at")] long start_y = 5) {
+        [Option("x", "x to start at")] long x = 5,
+        [Option("y", "y to start at")] long y = 5) {
 
         DiscordButtonComponent[] row1 = {
             new DiscordButtonComponent(ButtonStyle.Secondary, "disabled11", " ", true),
@@ -112,66 +52,47 @@ class PixelCommands : ApplicationCommandModule {
             new DiscordButtonComponent(ButtonStyle.Secondary, "disabled54", " ", true),
             new DiscordButtonComponent(ButtonStyle.Danger, "exit", null, false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":heavy_multiplication_x:")))
         };
-        DiscordSelectComponentOption[] colorOptions = new DiscordSelectComponentOption[Enum.GetValues(typeof(PixelModule.PixelEnum)).Length];
-        foreach(int i in Enum.GetValues(typeof(PixelModule.PixelEnum))) {
-            string name = Enum.GetName(typeof(PixelModule.PixelEnum), i);
-            colorOptions[i] = new DiscordSelectComponentOption(name, name);
-        }
+
+        List<DiscordSelectComponentOption> colorOptions = new();
+        ((int[])Enum.GetValues(typeof(PixelModule.PixelEnum))).ToList().ForEach(x => { 
+            string name = Enum.GetName(typeof(PixelModule.PixelEnum), x); 
+            colorOptions.Add(new DiscordSelectComponentOption(name, name));
+        });
+
         DiscordSelectComponent colorSelectComponent = new DiscordSelectComponent("color", "Select color to place:", colorOptions);
 
-        if(start_x < 0) {
-            start_x = 0;
-        }
-        if(start_x >= PixelModule.MapWidth) {
-            start_x = PixelModule.MapWidth - 1;
-        }
-        if(start_y < 0) {
-            start_y = 0;
-        }
-        if(start_y >= PixelModule.MapHeight) {
-            start_y = PixelModule.MapHeight - 1;
-        }
-
-        int x = (int)start_x;
-        int y = (int)start_y;
+        int curX = (int)Math.Clamp(x, 0, PixelModule.MapWidth - 1);
+        int curY = (int)Math.Clamp(y, 0, PixelModule.MapWidth - 1);
         int zoom = 9;
         int jumpAmount = 1;
-        string imageName = $"img{ctx.User.Id}.png";
         string imagePath = $"PixelImages/img{ctx.User.Id}.png";
-        PixelModule.PixelEnum selectedColorEnum = PixelModule.PixelEnum.White;
-
-        var embed = new DiscordEmbedBuilder {
-            Title = "Pixel",
-            Color = DefaultColor,
-            ImageUrl = $"attachment://{imageName}",
-            Description = $"{ctx.Guild.Name}'s canvas. ({x},{y}) is selected. {zoom} zoom. {jumpAmount} tiles per move."
-        };
+        PixelModule.PixelEnum curColor = PixelModule.PixelEnum.White;
 
         await ctx.CreateResponseAsync("Check DMs for an interactive canvas! (It may take some time to load)", true);
 
-        //Create the initial bitmap
-        SKSurface surface = GetPixelSurface(ctx, x - zoom / 2, y - zoom / 2, zoom);
-        AddInteractUI(surface, zoom, selectedColorEnum);
-        surface.Snapshot().SaveToPng(imagePath);
-
         DiscordMessage msg;
-        using(var fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read)) {
-            msg = await ctx.Member.SendMessageAsync(new DiscordMessageBuilder().AddComponents(row1).AddComponents(row2).AddComponents(row3).AddComponents(row4).AddComponents(row5).AddEmbed(embed).WithFile(imageName, fs));
-        }
+        var embed = new DiscordEmbedBuilder()
+            .WithTitle("Pixel")
+            .WithColor(DefaultColor)
+            .WithImageUrl($"attachment://{Path.GetFileName(imagePath)}");
+
         var interactivity = ctx.Client.GetInteractivity();
         while(true) {
-
+            embed.WithDescription($"{ctx.Guild.Name}'s canvas. ({curX},{curY}) is selected. {zoom} zoom. {jumpAmount} tiles per move.");
+            Bot.Modules.Pixel.CreateImageWithUI(ctx, curX, curY, zoom, curColor);
+            using(var fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read)) {
+                msg = await ctx.Member.SendMessageAsync(new DiscordMessageBuilder().AddComponents(row1).AddComponents(row2).AddComponents(row3).AddComponents(row4).AddComponents(row5).AddEmbed(embed).WithFile(Path.GetFileName(imagePath), fs));
+            }
             var input = await interactivity.WaitForButtonAsync(msg, ctx.User, timeoutOverride: TimeSpan.FromMinutes(1));
 
             if(input.TimedOut)
                 break;
             await input.Result.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage);
 
-            //This is some unoptimal shit, whatever
+            //From what I can tell there isn't a better way to do this :/
             if(input.Result.Id == "colorChange") {
                 await msg.DeleteAsync();
                 await Task.Delay(500);
-                embed.WithImageUrl("");
                 embed.WithDescription("");
                 msg = await ctx.Member.SendMessageAsync(new DiscordMessageBuilder().AddComponents(colorSelectComponent).AddEmbed(embed));
 
@@ -181,39 +102,37 @@ class PixelCommands : ApplicationCommandModule {
                     break;
                 await cInput.Result.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage);
                 if(Enum.TryParse(typeof(PixelModule.PixelEnum), cInput.Result.Values.First(), true, out var type)) {
-                    selectedColorEnum = (PixelModule.PixelEnum)type;
+                    curColor = (PixelModule.PixelEnum)type;
                 }
             }
             if(input.Result.Id == "exit") {
                 break;
             }
             if(input.Result.Id == "place") {
-                var map = Bot.Modules.Pixel.GetPixelMap(ctx.Guild.Id);
-                map.WritePixel(x, y, selectedColorEnum);
-                Bot.Modules.Pixel.SetPixelMap(map);
+                Bot.Modules.Pixel.PlacePixel(ctx.Guild.Id, curX, curY, curColor);
             }
             if(input.Result.Id == "moveUp") {
                 for(int i = 0; i < jumpAmount; i++) {
-                    if(y > 0)
-                        y--;
+                    if(curY > 0)
+                        curY--;
                 }
             }
             if(input.Result.Id == "moveDown") {
                 for(int i = 0; i < jumpAmount; i++) {
-                    if(y <= PixelModule.MapHeight)
-                        y++;
+                    if(curY <= PixelModule.MapHeight)
+                        curY++;
                 }
             }
             if(input.Result.Id == "moveLeft") {
                 for(int i = 0; i < jumpAmount; i++) {
-                    if(x > 0)
-                        x--;
+                    if(curX > 0)
+                        curX--;
                 }
             }
             if(input.Result.Id == "moveRight") {
                 for(int i = 0; i < jumpAmount; i++) {
-                    if(x <= PixelModule.MapHeight)
-                        x++;
+                    if(curX <= PixelModule.MapHeight)
+                        curX++;
                 }
             }
             if(input.Result.Id == "zoomIn") {
@@ -231,24 +150,14 @@ class PixelCommands : ApplicationCommandModule {
                 jumpAmount++;
             }
 
-            //Update and show image
-            surface = GetPixelSurface(ctx, x - zoom / 2, y - zoom / 2, zoom);
-            AddInteractUI(surface, zoom, selectedColorEnum);
-
-            surface.Snapshot().SaveToPng($"PixelImages/img{ctx.User.Id}.png");
-            embed.WithImageUrl($"attachment://img{ctx.User.Id}.png");
-            embed.WithDescription($"{ctx.Guild.Name}'s canvas. ({x},{y}) is selected. {zoom} zoom. {jumpAmount} tiles per move.");
-            using(var fs = new FileStream($"PixelImages/img{ctx.User.Id}.png", FileMode.Open, FileAccess.Read)) {
-                await msg.DeleteAsync();
-                await Task.Delay(500);
-                msg = await ctx.Member.SendMessageAsync(new DiscordMessageBuilder().AddComponents(row1).AddComponents(row2).AddComponents(row3).AddComponents(row4).AddComponents(row5).AddEmbed(embed).WithFile($"img{ctx.User.Id}.png", fs));
-            }
+            await msg.DeleteAsync();
+            await Task.Delay(500);
         }
 
         using(var fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read)) {
             await msg.DeleteAsync();
             embed.WithDescription($"{ctx.Guild.Name}'s canvas.");
-            await ctx.Member.SendMessageAsync(new DiscordMessageBuilder().AddEmbed(embed).WithFile(imageName, fs));
+            await ctx.Member.SendMessageAsync(new DiscordMessageBuilder().AddEmbed(embed).WithFile(Path.GetFileName(imagePath), fs));
         }
         File.Delete(imagePath);
     }
--- Source/Modules/PixelModule.cs
+++ Source/Modules/PixelModule.cs
@@ -1,13 +1,10 @@
 ﻿namespace DiscordBotRewrite.Modules;
 public class PixelModule {
-
-    Dictionary<ulong, PixelMap> PixelMaps;
-
+    #region Constants
     public const int MapWidth = 100;
     public const int MapHeight = 100;
-
     //Make sure to update both when adding a color
-    public static readonly Dictionary<uint, SKColor> PixelDict = new Dictionary<uint, SKColor>()
+    readonly Dictionary<uint, SKColor> PixelDict = new Dictionary<uint, SKColor>()
     {
         {0,  SKColors.White},
         {1,  SKColors.Red},
@@ -36,22 +33,100 @@ public class PixelModule {
         Black,
         Cyan
     };
+    #endregion
 
+    Dictionary<ulong, PixelMap> PixelMaps;
     public PixelModule() {
         PixelMaps = LoadJson<Dictionary<ulong, PixelMap>>(PixelMap.JsonLocation);
     }
 
-    public PixelMap GetPixelMap(ulong id) {
+    #region Public
+    public void PlacePixel(ulong guildId, int x, int y, PixelEnum color) {
+        var map = GetPixelMap(guildId);
+        map.PixelState[x, y] = color;
+        SavePixelMap(map);
+    }
+    public void CreateImage(InteractionContext ctx, int x, int y, int zoom) {
+        SKSurface surface = CreateSurface(GetPixelMap(ctx.Guild.Id), x - zoom / 2, y - zoom / 2, zoom);
+        surface.Snapshot().SaveToPng($"PixelImages/img{ctx.User.Id}.png");
+    }
+    public void CreateImageWithUI(InteractionContext ctx, int x, int y, int zoom, PixelEnum selectedPixel) {
+        SKSurface surface = CreateSurface(GetPixelMap(ctx.Guild.Id), x - zoom / 2, y - zoom / 2, zoom);
+        if(PixelDict.TryGetValue((uint)selectedPixel, out SKColor color))
+            DrawCenterPixel(surface.Canvas, zoom, color);
+        AddOutline(surface.Canvas, zoom, 2, SKColors.Silver);
+        surface.Snapshot().SaveToPng($"PixelImages/img{ctx.User.Id}.png");
+    }
+    #endregion
+
+    #region Private
+    void SavePixelMap(PixelMap data) {
+        PixelMaps.AddOrUpdate(data.Id, data);
+        SaveJson(PixelMaps, PixelMap.JsonLocation);
+    }
+    PixelMap GetPixelMap(ulong id) {
         if(!PixelMaps.TryGetValue(id, out PixelMap pixelMap)) {
             pixelMap = new PixelMap(id);
             PixelMaps.Add(id, pixelMap);
         }
         return pixelMap;
     }
-    public void SetPixelMap(PixelMap data) {
-        PixelMaps.AddOrUpdate(data.Id, data);
-        SaveJson(PixelMaps, PixelMap.JsonLocation);
+    SKSurface CreateSurface(PixelMap map, int anchorX, int anchorY, int distance) {
+        int scale = 500 / distance;
+
+        SKImageInfo imageInfo = new SKImageInfo(500, 500);
+        SKSurface surface = SKSurface.Create(imageInfo);
+        SKCanvas canvas = surface.Canvas;
+
+        for(int x = 0; x < distance; x++) {
+            for(int y = 0; y < distance; y++) {
+                int curX = x + anchorX;
+                int curY = y + anchorY;
+                SKColor color;
+                bool exists = true;
+                if(curX < 0 || curX >= MapWidth || curY < 0 || curY >= MapHeight) {
+                    color = SKColors.Black.WithAlpha(255);
+                    exists = false;
+                } else if(!PixelDict.TryGetValue((uint)map.PixelState[curX, curY], out color)) {
+                    color = SKColors.White;
+                }
+                SKPaint paint = new SKPaint();
+                paint.Color = color;
+                paint.Style = SKPaintStyle.Fill;
+                paint.IsAntialias = false;
+
+                if(exists)
+                    canvas.DrawRect(x * scale, y * scale, scale, scale, paint);
+                else
+                    canvas.DrawHatchedRect(x * scale, y * scale, scale, scale, paint, 5);
+            }
+        }
+
+        return surface;
     }
+    void AddOutline(SKCanvas canvas, int zoom, int thickness, SKColor color) {
+        SKPaint paint = new SKPaint();
+        paint.Color = color;
+        paint.Style = SKPaintStyle.Stroke;
+        paint.StrokeWidth = thickness;
+        paint.IsAntialias = false;
+
+        int pixelSize = 500 / zoom;
+
+        canvas.DrawRect(pixelSize * (zoom / 2), pixelSize * (zoom / 2), pixelSize, pixelSize, paint);
+    }
+    void DrawCenterPixel(SKCanvas canvas, int zoom, SKColor color) {
+        SKPaint paint = new SKPaint();
+        paint.Color = color;
+        paint.Style = SKPaintStyle.Stroke;
+        paint.IsAntialias = false;
+        paint.StrokeWidth = 1;
+
+        int pixelSize = 500 / zoom;
+
+        canvas.DrawHatchedRect(pixelSize * (zoom / 2), pixelSize * (zoom / 2), pixelSize, pixelSize, paint, 5);
+    }
+    #endregion
 
     public class PixelMap {
         public const string JsonLocation = "Json/PixelMaps.json";
@@ -65,13 +140,9 @@ public class PixelModule {
             PixelState = new PixelEnum[MapWidth, MapHeight];
             Id = id;
         }
-
-        public void WritePixel(int x, int y, PixelEnum color) {
-            PixelState[x, y] = color;
-        }
     }
 }
-public static class SKCanvasExtensions {
+public static class SKExtensions {
     public static void SaveToPng(this SKImage image, string path) {
         using(SKData data = image.Encode(SKEncodedImageFormat.Png, 100)) {
             if(!File.Exists(path))
@@ -81,5 +152,23 @@ public static class SKCanvasExtensions {
                 data.SaveTo(stream);
             }
         }
+    }
+    public static void DrawHatchedRect(this SKCanvas canvas, float x, float y, float w, float h, SKPaint paint, int hatchDistance) {
+        SKPath path = new SKPath();
+        for(int i = 0; i < w; i += hatchDistance) {
+            path.MoveTo(x + i, y);
+            float size = Math.Min(h,Math.Min(w, w - i));
+            path.LineTo(x + i + size, y + size);
+        }
+        for(int i = 0; i < h; i += hatchDistance) {
+            path.MoveTo(x, y + i);
+            float size = Math.Min(w,Math.Min(h, h - i));
+            path.LineTo(x + size, y + i + size);
+        }
+        //Hatching only works with stroke paint, temporarily set it to that to ensure proper drawing
+        SKPaintStyle oldStyle = paint.Style;
+        paint.Style = SKPaintStyle.Stroke;
+        canvas.DrawPath(path, paint);
+        paint.Style = oldStyle;
     }
 }
