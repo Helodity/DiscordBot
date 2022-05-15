cd0686eeaa2c0c3eddbe121be4d570f58b9be612
Helodity <43509068+Helodity@users.noreply.github.com>
2022-05-15 13:11:29 -0400

    Split PixelModule into multiple files

---
+++ Source/Extensions/SKExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace DiscordBotRewrite.Extensions;
+
+public static class SKExtensions {
+    public static void SaveToPng(this SKImage image, string path) {
+        using(SKData data = image.Encode(SKEncodedImageFormat.Png, 100)) {
+            if(!File.Exists(path))
+                FileExtension.CreateFileWithPath(path);
+
+            using(var stream = File.OpenWrite(path)) {
+                data.SaveTo(stream);
+            }
+        }
+    }
+    public static void DrawHatchedRect(this SKCanvas canvas, float x, float y, float w, float h, SKPaint paint, int hatchDistance) {
+        SKPath path = new SKPath();
+        for(int i = 0; i < w; i += hatchDistance) {
+            path.MoveTo(x + i, y);
+            float size = Math.Min(h,Math.Min(w, w - i));
+            path.LineTo(x + i + size, y + size);
+        }
+        for(int i = 0; i < h; i += hatchDistance) {
+            path.MoveTo(x, y + i);
+            float size = Math.Min(w,Math.Min(h, h - i));
+            path.LineTo(x + size, y + i + size);
+        }
+        //Hatching only works with stroke paint, temporarily set it to that to ensure proper drawing
+        SKPaintStyle oldStyle = paint.Style;
+        paint.Style = SKPaintStyle.Stroke;
+        canvas.DrawPath(path, paint);
+        paint.Style = oldStyle;
+    }
+}
---
+++ Source/Modules/Pixel/PixelMap.cs
@@ -0,0 +1,37 @@
+﻿namespace DiscordBotRewrite.Modules;
+using static DiscordBotRewrite.Modules.PixelModule;
+public class PixelMap {
+    public const string JsonLocation = "Json/PixelMaps.json";
+
+    [JsonProperty("guild_id")]
+    public readonly ulong Id;
+    [JsonProperty("width")]
+    public int Width;
+    [JsonProperty("height")]
+    public int Height;
+    [JsonProperty("pixel_data")]
+    public PixelEnum[,] PixelState;
+
+    public PixelMap(ulong id, int width = 100, int height = 100) {
+        Width = width;
+        Height = height;
+        PixelState = new PixelEnum[Width, Height];
+        Id = id;
+    }
+
+    public void ChangeSize(int width, int height) {
+        PixelEnum[,] old = PixelState;
+        int maxY = Math.Min(old.GetLength(1), height);
+        int maxX = Math.Min(old.GetLength(0), width);
+
+        Width = width;
+        Height = height;
+        PixelState = new PixelEnum[Width, Height];
+        for(int y = 0; y < maxY; y++) {
+            for(int x = 0; x < maxX; x++) {
+                PixelState[x, y] = old[x, y];
+            }
+        }
+    }
+
+}
